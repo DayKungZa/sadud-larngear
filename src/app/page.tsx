bf50c341b51e18a89e277f61482262db0b63b4f2
DayKungZa <tawanday2006@gmail.com>
2025-03-14 16:27:30 +0700

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import Banner from "@/components/Banner";
-import Card from "@/components/Card";
-import TopMenu from "@/components/TopMenu";
-import CardPanel from "@/components/CardPanel";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Banner />
-      <CardPanel/>
+      hello test
     </main>
   );
 }
