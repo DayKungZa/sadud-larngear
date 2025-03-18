dd7b4a2ad8cf6cfc6148bc26306a905f475032c7
Sirawit Petchsriudom <6733275621@student.chula.ac.th>
2025-03-18 18:21:47 +0700

    add fortune button

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -9,24 +9,24 @@ const Grid: React.FC<GridProps> = ({ onCellSelect }) => {
   const rows = "ABCDEF".split(""); // 6 rows (A-F)
   const cols = Array.from({ length: 13 }, (_, i) => i + 1); // 13 columns (1-13)
   const colors = useRef(new Map<string,number>())
-  
-  if(colors.current.size == 0)
-  for(let a of rows){
-    for(let b of cols){
-      colors.current.set(a+":"+b,Math.round(Math.random()*255));
+  const [update,setUpdate] = useState(true);
+  useEffect(()=>{
+    for(let a of rows){
+      for(let b of cols){
+        colors.current.set(a+":"+b,Math.round(Math.random()*255));
+      }
     }
-  }
+  },[update])
 
   
   return (
-    <div className="flex justify-center items-center h-full w-full">
-      <button>asds</button>
+    <div className="flex justify-center items-center h-full w-full" >
       <div className="relative bg-[url('/Larngear_crop.png')] bg-contain p-2 rounded-lg shadow-lg w-full h-full">
         {/* Full Grid Container */}
         <div className="grid w-full h-full" style={{ gridTemplateColumns: "auto repeat(13, 1fr)", gridTemplateRows: "auto repeat(6, 1fr)" }}>
           
-          {/* Top-left empty space for alignment */}
-          <div className="w-16 h-16"></div>
+          {/* Top-left button for fortune */}
+          <button className="w-16 h-16 rouneded-xl border bg-gray-600 hover:bg-gray-700 text-sm"onClick={()=>setUpdate(!update)}>{"Sadud Fortune"}</button>
 
           {/* Column Headers (1-13) */}
           {cols.map((col) => (
